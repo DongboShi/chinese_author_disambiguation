2d2dae55d2c87d7d95b7b02115c1af6cf68540fb
Lemon-mengmeng <lemon-meng@sjtu.edu.cn>
2019-10-30 14:55:27 +0800

    fix:all feature in a dataframe

--- feature_aff.R
+++ feature_aff.R
@@ -8,14 +8,13 @@ library(parallel)
 library(readr)
 # ptm <- proc.time()
 rm(list=ls())
-setwd("D:/")
+setwd('D:/0LLLab/chinese_author_disambiguation')
 # setwd("/Users/birdstone/Documents/Data")
 # h5read(pair[c("paperA","paperB")],file=paste0("/Users/zijiangred/changjiang/dataset/pairorder/",
 #                                               i,"_pair.h5"),name="pair")
 i = 1
 pairorder <- h5read(file=paste0(i,"_pair.h5"),name="pair")
 # data <- fromJSON(file=paste0("/Users/zijiangred/changjiang/dataset/inputdata/",i,".json"),simplify=T)
-i=1
 data <- fromJSON(file=paste0("CJ_",i,".json"),simplify=T)
 papers <- data$papers
 # All ut affliation
@@ -32,22 +31,23 @@ for(k in 1:length(papers)){
 }
 
 AFF <- unnest(aff,aff) %>%
+        mutate(aff = tolower(aff)) %>% 
         mutate(org1 = str_extract(aff,'[^,]+(?=,)')) %>%
         mutate(org2 = str_extract(aff,'([^,]+,[^,]+)(?=,)'))
-pairorderAB_length <- count(AFF,ut)
-colnames(pairorderAB_length) <- c('ut',"num")
 
-AFF <- left_join(AFF,pairorderAB_length)
+org1_count <- group_by(AFF,ut,org1) %>%
+    count()
+AFF <- left_join(AFF,org1_count)
+colnames(AFF) <- c("ut","aff","org1","org2","org1_count")
+org2_count <- group_by(AFF,ut,org2) %>%
+    count()
+AFF <- left_join(AFF,org2_count)
+colnames(AFF) <- c("ut","aff","org1","org2","org1_count","org2_count")
 
-# feature_aff11 <- select(pairorder,paperA,paperB,Aorg1,Borg1)
-# for(i in 1:dim(pairorder)[1]){
-#     feature_aff11$aff11[i] <- length(intersect(feature_aff11$Aorg1[[i]],feature_aff11$Borg1[[i]]))/length(union(feature_aff11$Aorg1[[i]],feature_aff11$Borg1[[i]]))
-# }
-# 
-# feature_aff12 <- select(pairorder,paperA,paperB,Aorg2,Borg2)
-# for(i in 1:dim(pairorder)[1]){
-#     feature_aff12$aff12[i] <- length(intersect(feature_aff12$Aorg2[[i]],feature_aff12$Borg2[[i]]))/length(union(feature_aff12$Aorg2[[i]],feature_aff12$Borg2[[i]]))
-# }
+ogr_length <- count(AFF,ut)
+colnames(ogr_length) <- c('ut',"org_count")
+
+AFF <- left_join(AFF,ogr_length)
 
 ############################################################
 # make idf
@@ -66,71 +66,45 @@ GlobalAFF2 <- read_csv('org2_tf.csv') #read.csv 太慢了
 GlobalAFF2_sum <- sum(GlobalAFF2$frequency)
 GlobalAFF2 <- mutate(GlobalAFF2,idf_aff2 = log(GlobalAFF2_sum/frequency))
 
+# match info on paper
 pairorderA <- left_join(pairorder,AFF,by=c('paperA'='ut'))
-colnames(pairorderA) <- c("paperA","paperB","aff","org1","org2","numA")
+colnames(pairorderA) <- c("paperA","paperB","aff","org1","org2","org1_countA",
+                          "org2_countA","org_countA")
 pairorderB <- left_join(pairorder,AFF,by=c('paperB'='ut'))
-colnames(pairorderB) <- c("paperA","paperB","aff","org1","org2","numB")
+colnames(pairorderB) <- c("paperA","paperB","aff","org1","org2","org1_countB",
+                          "org2_countB","org_countB")
 
 pairorderA_pairorderB_intersectorg1 <- inner_join(select(pairorderA,-aff,-org2),
                                                   select(pairorderB,-aff,-org2)) %>% distinct() 
-pairorderA_pairorderB_intersectorg1 <- select(left_join(pairorderA_pairorderB_intersectorg1,part_aff1),-n)
+pairorderA_pairorderB_intersectorg1 <- select(left_join(pairorderA_pairorderB_intersectorg1,
+                                                        part_aff1),-n,-org2_countA,-org2_countB)
 pairorderA_pairorderB_intersectorg1 <- select(left_join(pairorderA_pairorderB_intersectorg1,
                                                         GlobalAFF1,by=c('org1'='term')),-frequency)
+pairorderA_pairorderB_intersectorg1 <- mutate(pairorderA_pairorderB_intersectorg1,
+                                              org1_part_idf_aff1=part_idf_aff1*pmin(org1_countA,org1_countB),
+                                              org1_idf_aff1=idf_aff1*pmin(org1_countA,org1_countB),
+                                              org1_aff11 = pmin(org1_countA,org1_countB)/(org_countA+org_countB))
 
 pairorderA_pairorderB_intersectorg2 <- inner_join(select(pairorderA,-aff,-org1),
                                                   select(pairorderB,-aff,-org1)) %>% distinct() 
-pairorderA_pairorderB_intersectorg2 <- select(left_join(pairorderA_pairorderB_intersectorg2,part_aff2),-n)
+pairorderA_pairorderB_intersectorg2 <- select(left_join(pairorderA_pairorderB_intersectorg2,
+                                                        part_aff2),-n,-org1_countA,-org1_countB)
 pairorderA_pairorderB_intersectorg2 <- select(left_join(pairorderA_pairorderB_intersectorg2,
                                                         GlobalAFF2,by=c('org2'='term')),-frequency)
+pairorderA_pairorderB_intersectorg2 <- mutate(pairorderA_pairorderB_intersectorg2,
+                                              org2_part_idf_aff2=part_idf_aff2*pmin(org2_countA,org2_countB),
+                                              org2_idf_aff2=idf_aff2*pmin(org2_countA,org2_countB),
+                                              org2_aff12 = pmin(org2_countA,org2_countB)/(org_countA+org_countB))
 
-pairorder_idf1 <- group_by(pairorderA_pairorderB_intersectorg1,paperA,paperB) %>%
-    summarise(aff21 = sum(part_idf_aff1),aff31 = sum(idf_aff1))
+# min is wrong, use pmin :https://dennisphdblog.wordpress.com/2009/07/24/r-command-of-the-week-pmax-and-pmin/
+pairorder_org1 <- group_by(pairorderA_pairorderB_intersectorg1,paperA,paperB) %>%
+    summarise(aff11 = sum(org1_aff11),aff21 = sum(org1_part_idf_aff1),aff31 = sum(org1_idf_aff1))
 
-pairorder_idf2 <- group_by(pairorderA_pairorderB_intersectorg2,paperA,paperB) %>%
-    summarise(aff22 = sum(part_idf_aff2),aff32 = sum(idf_aff2))
-
-pairorder_Aff2 <- left_join(pairorder,pairorder_idf1)
-pairorder_Aff3 <- left_join(pairorder,pairorder_idf2)
-
-#######################
-# make feature feature_aff11, feature_aff12
-
-Affiliation <-  AFF %>%
-    group_by(ut) %>%
-    summarise(org1=paste(org1,collapse='---'),org2=paste(org2,collapse='---'))%>%
-    mutate(org1=str_split(org1,"---"),org2=str_split(org2,"---"))
-Affiliation <- left_join(Affiliation,pairorderAB_length)
-
-pairorder <- left_join(pairorder,Affiliation,by=c('paperA'='ut'))
-colnames(pairorder) <- c("paperA","paperB","Aorg1","Aorg2","numA")
-pairorder <- left_join(pairorder,Affiliation,by=c('paperB'='ut'))
-colnames(pairorder) <- c("paperA","paperB","Aorg1","Aorg2","numA","Borg1","Borg2","numB")
-
-pairorderAB_intersectorg1_count <- group_by(pairorderA_pairorderB_intersectorg1,
-                                            paperA,paperB) %>%count()
-pairorder_Aff1 <- left_join(pairorder,pairorderAB_intersectorg1_count)
-colnames(pairorder_Aff1) <- c("paperA","paperB","Aorg1","Aorg2","numA","Borg1","Borg2","numB","org1_count")
-pairorderAB_intersectorg2_count <- group_by(pairorderA_pairorderB_intersectorg2,
-                                            paperA,paperB) %>%count()
-pairorder_Aff1 <- left_join(pairorder_Aff1,pairorderAB_intersectorg2_count)
-colnames(pairorder_Aff1) <- c("paperA","paperB","Aorg1","Aorg2","numA","Borg1","Borg2","numB","org1_count","org2_count")
-
-pairorder_Aff1[is.na(pairorder_Aff1)] <- 0
-pairorder_Aff1 <- mutate(pairorder_Aff1,aff11=org1_count/(numA+numB),
-                         aff12=org2_count/(numA+numB))
-# all aff feature in dataframe  pairorder_Aff
-pairorder_Aff <- cbind(select(pairorder_Aff1,paperA,paperB,Aorg1,Borg1,Aorg2,
-                                  Borg2,numA,numB,org1_count,org2_count,
-                                  aff11,aff12),
-                       select(pairorder_Aff2,paperA,paperB,aff21,aff31),
-                       select(pairorder_Aff3,paperA,paperB,aff22,aff32))
-
-feature_aff11 <- select(pairorder_Aff1,paperA,paperB,aff11)
-feature_aff12 <- select(pairorder_Aff1,paperA,paperB,aff12)
+pairorder_org2 <- group_by(pairorderA_pairorderB_intersectorg2,paperA,paperB) %>%
+    summarise(aff12 = sum(org2_aff12),aff22 = sum(org2_part_idf_aff2),aff32 = sum(org2_idf_aff2))
 
 
-#调整feature的顺序使得其与原始的pair相一致
-# pairorder <- pairorder %>%
-#     arrange(match(paperA,pairorder_orig$paperA),
-#             match(paperB,pairorder_orig$paperB))
-
+pairorder_Aff <- left_join(pairorder_org1,pairorder_org2)
+Feature_aff <- left_join(pairorder,pairorder_Aff)
+Feature_aff[is.na(Feature_aff)] <- 0
+Feature_aff <- select(Feature_aff,paperA,paperB,aff11,aff12,aff21,aff22,aff31,aff32)
