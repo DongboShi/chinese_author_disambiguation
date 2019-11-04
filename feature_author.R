7923bad68b8779c8e2160558b6cd6b1405752a29
Lemon-mengmeng <lemon-meng@sjtu.edu.cn>
2019-11-04 10:23:55 +0800

    fix:last version

--- feature_author.R
+++ feature_author.R
@@ -6,37 +6,40 @@ library(rlist)
 library(stringr)
 library(parallel)
 library(readr)
-ptm <- proc.time()
+# ptm <- proc.time()
 # rm(list=ls())
-setwd('D:/0LLLab/chinese_author_disambiguation')
-# setwd("/Users/birdstone/Documents/Data")
-# h5read(pair[c("paperA","paperB")],file=paste0("/Users/zijiangred/changjiang/dataset/pairorder/",
-#                                               i,"_pair.h5"),name="pair")
-files <- list.files(pattern = "json")
+# setwd('D:/0LLLab/chinese_author_disambiguation')
+files <- list.files(path='/Users/zijiangred/changjiang/dataset/inputdata',pattern='CJ_')
+id <- sort(as.numeric(str_extract(files,'[0-9]+')))
+
 part_focus_name <- c()
-for (i in 1:length(files)){
-    data <- fromJSON(file=paste0("CJ_",i,".json"),simplify=T)
+for (i in id){
+    data <- fromJSON(file=paste0("/Users/zijiangred/changjiang/dataset/inputdata/CJ_",i,".json"),simplify=T)
     papers <- data$papers
     FocusName <- c()
     for(k in 1:length(papers)){
-        name <- data[["papers"]][[k]][["FocusName"]]
+        name <- data[["papers"]][[k]][["FocusName"]][1]
         FocusName <- c(FocusName,name)
     }
     part_focus_name <- c(part_focus_name,FocusName)
+    print(i)
 }
 # make idf
-IDFlName <- as.data.frame(table(str_extract(part_focus_name,'\\S+'))) 
+IDFlName <- as.data.frame(table(str_extract(part_focus_name,'\\S+')))
 colnames(IDFlName) <- c('lName','freq')
 IDFlName <- mutate(IDFlName,lName_idf = log(sum(freq)/freq))
+write.csv(IDFlName,file='/Users/zijiangred/changjiang/dataset/feature/IDFlName_idf.csv',row.names = F,na ='')
 
-for (i in 1:length(files)){
-    pairorder <- h5read(file=paste0(i,"_pair.h5"),name="pair")
-    data <- fromJSON(file=paste0("CJ_",i,".json"),simplify=T)
+# IDFlName <- read.csv('/Users/zijiangred/changjiang/dataset/feature/IDFlName_idf.csv')
+
+for (i in id){
+    pairorder <- h5read(file=paste0("/Users/zijiangred/changjiang/dataset/pairorder/",i,"_pair.h5"),name="pair")
+    data <- fromJSON(file=paste0("/Users/zijiangred/changjiang/dataset/inputdata/CJ_",i,".json"),simplify=T)
     papers <- data$papers
     FocusName <- data.frame()
     for(k in 1:length(papers)){
         ut <- papers[[k]]$UT
-        name <- data[["papers"]][[k]][["FocusName"]]
+        name <- data[["papers"]][[k]][["FocusName"]][1]
         result<-data.frame()
         if(length(name)>0){
             result <- data.frame(ut,stringsAsFactors = F)
@@ -60,7 +63,7 @@ for (i in 1:length(files)){
         mutate(givenname = ifelse(fNameA==fNameB & fullname==0,2,givenname)) %>%
         mutate(givenname = ifelse(fNameA==fNameB & fullname==2,3,givenname))
     
-    write.csv(Feature_givenname,file=paste0('Feature_givenname_',i,'.csv'),row.names = F,na='')
+    write.csv(Feature_givenname,file=paste0('/Users/zijiangred/changjiang/dataset/feature/feature_author/Feature_givenname/Feature_givenname_',i,'.csv'),row.names = F,na='')
     
     ############################################################
     pairorderAB_A <- select(left_join(select(pairorderAB,paperA,paperB,lNameA,lNameB),IDFlName,
@@ -69,7 +72,7 @@ for (i in 1:length(files)){
     pairorderAB_AB <- select(left_join(pairorderAB_A,IDFlName,by=c('lNameB'='lName')),-freq)
     colnames(pairorderAB_AB) <- c('paperA','paperB','lNameA','lNameB','lName_idfA','lName_idfB')
     Feature_lName <- mutate(pairorderAB_AB,IDF_lname=lName_idfA+lName_idfB)
-    write.csv(Feature_lName,file=paste0('Feature_lName',i,'.csv'),row.names = F,na='')
+    write.csv(Feature_lName,file=paste0('/Users/zijiangred/changjiang/dataset/feature/feature_author/Feature_lName/Feature_lName_',i,'.csv'),row.names = F,na='')
    
      ############################################################
     
@@ -100,7 +103,8 @@ for (i in 1:length(files)){
         mutate(authororder = ifelse(Focus_seqA ==1 & lastauthorB==1,1,authororder)) %>%
         mutate(authororder = ifelse(lastauthorA ==1 & Focus_seqB==1,1,authororder))
 
-    write.csv(Feature_authororder,file=paste0('Feature_authororder_',i,'.csv'),row.names = F,na ='')
-}
+    write.csv(Feature_authororder,file=paste0('/Users/zijiangred/changjiang/dataset/feature/feature_author/Feature_authororder/Feature_authororder_',i,'.csv'),row.names = F,na ='')
+    print(i)
+    }
 
-proc.time()-ptm
+# proc.time()-ptm
